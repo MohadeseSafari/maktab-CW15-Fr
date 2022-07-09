7d532e6747bae37aaf73e0648a07745fe4799b52
mohadese <mohadesesafari1381@gmail.com>
2022-07-09 13:00:08 +0430

    add base of Progect

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+// import Container from "./components/ex01/Container";
+import Card from "./components/ex02/Card";
+// import Time from "./components/ex03/Time";
+
+class App extends Component {
+  render() {
+    return <Card/>;
+  }
+}
+
+export default App;
